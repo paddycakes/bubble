7ad543ead65bae8b0051fee19eb57c270d316c85
Patrick Gallagher <paddy.gallagher@gmail.com>
2016-09-21 14:10:26 +0100

    Prototyping websocket functionality on server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
-var express = require('express');
-// var compression = require('compression');
-// var path = require('path');
-
+const express = require('express');
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 const port = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+    console.log('new connection made');
+
+    socket.emit('message-from-server', {
+        greeting: 'Hello from Server',
+    });
+
+    socket.on('message-from-client', (msg) => {
+        console.log(msg);
+    });
+});
 
 app.listen(port, () => {
     console.log('Listening on port ' + port);

16de927391987ee7d4b974e7b9376e402f8748e7
Patrick Gallagher <paddy.gallagher@gmail.com>
2016-09-23 08:25:16 +0100

    Capitalizing top level component name

--- client/index.ios.js
+++ client/index.ios.js
@@ -12,7 +12,7 @@ import {
   View
 } from 'react-native';
 
-class bubble extends Component {
+class Bubble extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -50,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('bubble', () => bubble);
+AppRegistry.registerComponent('Bubble', () => Bubble);

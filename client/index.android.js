8ecd11185657aff39d1cd44a057b0cb6f847e04f
Patrick Gallagher <paddy.gallagher@gmail.com>
2016-09-23 13:06:14 +0100

    Updating index.android.js to show basic Hello World

--- client/index.android.js
+++ client/index.android.js
@@ -1,53 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
 } from 'react-native';
 
-class Bubble extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    text: {
+        color: 'black',
+        backgroundColor: 'white',
+        fontSize: 30,
+        margin: 80,
+    },
 });
+
+class Bubble extends Component {
+    render() {
+        return React.createElement(Text, { style: styles.text }, 'Hello World! (Again)');
+    }
+}
 
 AppRegistry.registerComponent('Bubble', () => Bubble);

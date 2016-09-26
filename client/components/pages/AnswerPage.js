8375fb840cddb471298ea750e91f4d2e9c72611e
Patrick Gallagher <paddy.gallagher@gmail.com>
2016-09-26 13:29:22 +0100

    Adding ListView component for answers

--- client/components/pages/AnswerPage.js
+++ client/components/pages/AnswerPage.js
@@ -2,7 +2,6 @@ import React, { Component, PropTypes } from 'react';
 import {
     StyleSheet,
     Text,
-    TextInput,
     View,
     ListView,
     TouchableHighlight,
@@ -12,16 +11,60 @@ import {
 const styles = StyleSheet.create({
     description: {
         marginTop: 80,
-        marginBottom: 20,
         fontSize: 18,
         textAlign: 'center',
         color: '#656565',
+    },
+    textContainer: {
+        flex: 1,
+    },
+    separator: {
+        height: 1,
+        backgroundColor: '#dddddd',
+    },
+    answer: {
+        fontSize: 15,
+        color: '#656565',
+    },
+    user: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        color: '#48BBEC',
+    },
+    rowContainer: {
+        flexDirection: 'row',
+        padding: 10,
     },
 });
 
 class AnswerPage extends Component {
     constructor(props) {
         super(props);
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.state = {
+            answers: [],
+            dataSource: ds.cloneWithRows([{ id: 1, qId: 1, user: 'paddy', answer: 'At the corner of South and West Streets' },
+                                          { id: 2, qId: 1, user: 'piotr', answer: 'Up the road at Petticoat Lane' },
+                                          { id: 3, qId: 1, user: 'lloyd', answer: 'On Blueberry Parade' }]),
+        };
+        this.renderRow = this.renderRow.bind(this);
+    }
+
+    renderRow(rowData, sectionID, rowID) {
+        return (
+            <TouchableHighlight onPress={() => this.rowPressed(rowData.id)}
+                underlayColor='#dddddd'>
+                <View>
+                    <View style={styles.rowContainer}>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.answer}>{rowData.answer}</Text>
+                            <Text style={styles.user}>{rowData.user}</Text>
+                        </View>
+                    </View>
+                    <View style={styles.separator}/>
+                </View>
+            </TouchableHighlight>
+          );
     }
 
     render() {
@@ -29,13 +72,16 @@ class AnswerPage extends Component {
         return (
             <View>
                 <Text style={styles.description}>{this.props.question.text}</Text>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={this.renderRow}/>
             </View>
         );
     }
 }
 
 AnswerPage.propTypes = {
-    question: React.PropTypes.object.isRequired,
+    question: PropTypes.object.isRequired,
 };
 
 export default AnswerPage;
